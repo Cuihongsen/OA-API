0609462e52c1a4bf7840db7373d02fa6b35e5a49
崔宏森 <948346354@qq.com>
2016-10-24 16:42:45 +0800

    debug

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,6 +7,7 @@ return array(
     'DB_PWD'       => 'cui19950422', // 密码
     'DB_PORT'      => '3306', // 端口
     'DB_PREFIX'    => 'oa_', // 数据库表前缀
+    
 
     'URL_MODEL'    => 2, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
