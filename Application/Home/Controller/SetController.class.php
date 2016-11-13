0f8741c8622c81ed35861310d358e409454974ef
崔宏森 <948346354@qq.com>
2016-11-14 07:55:54 +0800

    saDASDASD

--- Application/Home/Controller/SetController.class.php
+++ Application/Home/Controller/SetController.class.php
@@ -66,7 +66,7 @@ class SetController extends BasicController
         $deleteLsit      = array();
         $companyModel    = D('company');
         $company         = $companyModel->where(array('c_id' => $this->user['c_id']))->find();
-        $dates           = week_to_date(array(0, 6), date('Y-m-d H:i:s', time() + 24 * 60 * 60), $company['end_time']);
+        $dates           = week_to_date(array(I('post.date')[0],I('post.date')[1]), date('Y-m-d H:i:s', time() + 24 * 60 * 60), $company['end_time']);
         $dataList        = array();
         $sql             = "SELECT * from oa_company LEFT  JOIN oa_user on oa_company.c_id=oa_user.c_id WHERE oa_company.c_id= " . $this->user['c_id'];
         $users           = D()->query($sql);
